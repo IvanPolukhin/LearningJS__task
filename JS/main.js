8fd9d45e70b66603c9fcf70f842e9cab51947ebe
IvanPolukhin <ivan.poluhin.200407@gmail.com>
2023-10-26 21:29:55 +0300

    while and For

---
+++ JS/main.js
@@ -0,0 +1,101 @@
+// alert("i JS")
+
+// alert(2+2+1)
+
+// let name = 'vlad';
+// let admin = name;
+// console.log(admin);
+
+// let earth;
+// let person;
+
+// const birthday = '18.04.1982';
+
+// const age = someCode(birthday);
+
+// let name = prompt('Enter your name');
+
+// alert(`Hello ${name}`);
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+// alert(+a + +b);
+
+// alert("2" > "12")
+
+// let js = prompt('Какое "официальное" название JavaScript?', '');
+
+// if (js == 'ECMAScript') {
+//     alert('Right');
+// } else {
+//     alert('You dont know? "ECMAScript"!');
+// }
+
+// let num = prompt('Enter the number');
+
+// if (num > 0) {
+//     alert(1);
+// } else if (num < 0) {
+//     alert(-1);
+// } else{
+//     alert(0);
+// }
+
+// let result = (a + b < 4) ? 'little' : 'much';
+
+// let login = prompt('login?');
+
+// (login == 'Employee') ? alert('hello') :
+//     (login == 'Director') ? alert('Hello') :
+//         (login == '') ? alert('No login') : alert('');
+
+// let age = prompt('Enter a number');
+// if (age < 14 || age > 90) {
+//     alert(age + ' подходит');
+// } else if (age >= 14 && age <= 90) {
+//     alert(age + ' не подходит');
+// }
+
+// let log = prompt('Who is there?');
+
+// if (log == "Admin") {
+//     let pass = prompt('password?');
+//     if (pass == "I'm in charge") {
+//         alert('Hello!');
+//     } else if (pass == null || pass == '') {
+//         alert('Canceled');
+//     } else {
+//         alert('Wrong password');
+//     }
+// } else if (log == null || log == '') {
+//     alert('Canceled');
+// } else {
+//     alert("I don't know you");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 == 0) {
+//         alert(i);
+//     }
+// }
+
+// let i = 0;
+// while (i < 3) {
+//     alert(`number ${i}!`);
+//     i++
+// }
+
+// let imput;
+// do {
+//     imput = prompt('ввести число, большее 100.');
+// } while (imput <= 100 && imput);
+
+let n = 10;
+
+for (i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+
+}
